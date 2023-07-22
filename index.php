60f9348a38059bebfb1dc4d2309cd00b58bae1b9
naber <98043924+naberxdxd@users.noreply.github.com>
2023-07-22 18:38:16 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,21 +1,23 @@
-<head>
-<meta charset="UTF-8">
-<meta name="description" content="Free Web tutorials">
-<meta name="keywords" content="HTML,CSS,XML,JavaScript">
-<meta name="author" content="John Doe">
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Iceland">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Hacked by İllegalMedya.Net</title>
-</head><br><br><br><br>
-<div align="center"><img style="padding:20;" align="center" src="https://i.hizliresim.com/1bhzy5w.png" alt="resim"></div>
-<br>
-<body bgcolor="#000">
-<div align="center"> <p> <br> <img src="https://i.hizliresim.com/jj1jmD.gif" width="300" height="23" style="margin-left:0px;"> <br><font face="Iceland" size="8" color="#FFFFFF">Hacked By Musto - Regex - Areus - Vitho - Vixyum - Destroyer</font><br><font face= <img src="https://i.hizliresim.com/jj1jmD.gif" width="300" height="23" style="margin-left:0px;"> <br> <font face="Iceland" size="5" color="#CCCCCC"> ~ İcraat Ddos ile Olmaz Kitabınızı Sikerim Böyle Yokolun ~ <br><br> <br><br><br>[ illegalmedya.net ]</font><br></font> <font color="#CCCCCC" face="Trebuchet MS"></font></div>
-<div align="center"> <p> <font face="Iceland" size="10" color="red"> <br><br> Yönlendiriliyorsunuz Bekleyin.
-<div align="center"> <p> <font face="Iceland" size="5" color="#FFFFFF"> Emeği Geçen Herkese Teşekkürler İllegalMedya.Net Türeme Siktertmece ONLİNE XD<br><br>
-<br><br>
-<iframe type="text/html" width="0" height="0" src=]" frameborder="0" allow="autoplay"></iframe>
-</body>
-</html>
+<html lang=”tr”><head>
 
-<meta http-equiv="refresh" content="9;URL=https://illegalmedya.net">
+    <title>Hacked By Regex</title>
+    </head>
+    
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <body bgcolor="black" style="color:dark"><center>
+    <br><br><br>
+    <img src="https://cdn.discordapp.com/attachments/1094387576032465046/1094601469115760770/222.png">
+    <br><br><br>
+    
+    <font face="Courier New" color="red" size="9"><span style="text-shadow: 0px 0px 9px ;"><strong>hacked by regex</strong></span></font>
+    
+    <br><br><hr width="9%"><br><br>
+    
+    <font face="Courier New" color="red" size="5"><span style="text-shadow: 0px 0px 9px ;"><strong>Hack</strong> bir <strong>zanaattir zanaat</strong> ise bir <strong>yetenektir! </strong></span></font>&nbsp;&nbsp;<font face="Courier New" color="red" size="5"></font>
+ <br><br>   
+    <font face="Courier New" color="red" size="6">illegalmedya.net Sundu. <br> 
+    
+    <br><hr width="9%"><br>
+    
+
+    </font></font></font></center></body></html>
