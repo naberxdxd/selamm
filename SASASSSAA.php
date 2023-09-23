a0773e71dfc06f5834396fbb7389a534ead37a68
naber <98043924+naberxdxd@users.noreply.github.com>
2023-09-24 02:11:26 +0300

    Update and rename SASASA.php to SASASSSAA.php

---
+++ SASASSSAA.php
@@ -0,0 +1,7 @@
+<html><head><title>HACKED | REGEX & VOIDSPLOIT</title>
+<meta charset="utf-8">
+</head><body bgcolor="black">
+<p style="text-align:center"><span style="font-size:26px"><span style="font-family:Verdana,Geneva,sans-serif"><strong><span style="color:#ff0000">Hacked by</span> <span style="color:#ff0000">REGEX & HEGEMON</strong></span></span></p>
+<p style="text-align:center"><span style="color:#ffffff"><strong></strong></span></p>
+<center><iframe width="300" height="200" src="https://www.youtube.com/embed/sFaRrnE4ZWQ" title="22 Ağustos 2023" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></center>
+</body></html>
